132fa9b29c8adb3f7fbf897ad70b078b81554a48
singhasla <s1n9hasla@gmail.com>
2021-02-05 23:56:47 +0900

    메인화면 설정

--- ShoppingMall/src/controller/ShoppingController.java
+++ ShoppingMall/src/controller/ShoppingController.java
@@ -1,5 +1,11 @@
 package controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import model.MemberBean;
 
 public class ShoppingController {
 
